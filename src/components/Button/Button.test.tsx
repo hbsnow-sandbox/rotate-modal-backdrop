6f07a1cd097ac4b59390bccf72710b0028647049
Takahashi Yuki <yuki.4uing@gmail.com>
2022-10-11 06:40:57 +0900

    Initial commit

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+
+import { Button } from ".";
+
+describe("Button Component", () => {
+  it("render the container", () => {
+    render(<Button>button</Button>);
+
+    const target = screen.getByTestId("button");
+    expect(target).toHaveTextContent("button");
+  });
+});
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import { ComponentPropsWithoutRef, forwardRef, PropsWithChildren } from "react";
+
+export type Props = Readonly<
+  PropsWithChildren<Omit<ComponentPropsWithoutRef<"button">, "className">>
+>;
+
+export const Button = forwardRef<HTMLButtonElement, Props>((props, ref) => {
+  return <button ref={ref} data-testid="button" {...props} />;
+});
+Button.displayName = "Button";
